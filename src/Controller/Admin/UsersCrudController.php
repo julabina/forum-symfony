3ad08f8664f260e933ce74215edf40f6f90b5d11
Lenfumé Julien <julabina@hotmail.fr>
2022-08-29 18:23:44 +0200

    after symfony update 5.4 to 6.0, install easyAdmin + basics easy admin controller

---
+++ src/Controller/Admin/UsersCrudController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Users;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class UsersCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Users::class;
+    }
+
+    /* public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Recettes')
+            ->setEntityLabelInSingular('Recette')
+
+            ->setPageTitle('index', "TITRE DES RECETTES")
+
+            ->setPaginatorPageSize(10)
+            ;
+
+    } */
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
